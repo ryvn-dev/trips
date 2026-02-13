f8a98973fcf536fc81605151a8da42de4c1e885e
ryvn-dev <austenpsy@gmail.com>
2026-02-13 15:56:09 +0800

    Comment out eruda dev console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,12 +37,12 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        {process.env.NODE_ENV === "development" && (
+        {/* {process.env.NODE_ENV === "development" && (
           <>
             <script src="https://cdn.jsdelivr.net/npm/eruda" />
             <script dangerouslySetInnerHTML={{ __html: "eruda.init();" }} />
           </>
-        )}
+        )} */}
       </body>
     </html>
   );
